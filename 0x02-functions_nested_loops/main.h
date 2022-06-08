60ea74c352dd32f6079f53c145bd1a055e1941f0
Bello Shukroh <adetejubello@gmail.com>
2022-06-08 21:26:00 +0000

    commit 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,11 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
-/* MAIN_H */
-
+/**
+ *  MAIN_H
+ *  _putchar - writes the character c to std
+ *  Return: 1 0r 0
+ */
 int _putchar(char c);
 
 #endif
