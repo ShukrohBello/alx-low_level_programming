e0799653e6fecf56a088aaa3dbb793851e22d74e
Bello Shukroh <adetejubello@gmail.com>
2022-06-09 13:36:12 +0000

    add

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - function that prints the numbers, from 0 to 9, followed by a new line.
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+}
