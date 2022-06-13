c38e50d935dd399ca18d049d0a6e81b8aed7178e
Bello Shukroh <adetejubello@gmail.com>
2022-06-13 16:00:51 +0000

    corrected task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,13 @@
  */
 void print_rev(char *s)
 {
-	int i =0;
+	int i = 0;
 
-	while (s[i] = !='\0')
+	while (s[i] != '\0')
 		i++;
 
-	for (i= i - 1; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 		_putchar(s[i]);
 
-	_pitchar(s[i]);
+	_putchar(s[i]);
 }
