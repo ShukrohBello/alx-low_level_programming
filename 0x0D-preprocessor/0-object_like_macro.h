86c11ce5d2a448e828fdea435cc9d21479383c94
Bello Shukroh <adetejubello@gmail.com>
2022-07-04 15:29:14 +0000

    Task 1 and 2

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+/*
+ * @File: 0-object_like_macro.
+ * a header file that defines a macro named SIZE
+ * as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
