f6dba788667c80914f115f8efc5dee0621d1353f
Bello Shukroh <adetejubello@gmail.com>
2022-09-22 11:50:46 +0000

    tasks 0 - 6

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - return the index at which a key/value pair
+ *	should be stored in array of a hash table
+ * @key: The key to get the index of.
+ * @size: The size of the array of the hash table.
+ *
+ * Return: The index of the key Description: uses djb2 algorithm.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_dj2(key) % size);
+}
+
