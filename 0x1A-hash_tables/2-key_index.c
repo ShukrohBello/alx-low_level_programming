15ac1a87dca244e5252cca6aa8378c048880cb8a
Bello Shukroh <adetejubello@gmail.com>
2022-09-23 15:10:39 +0000

    corrections

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,10 +6,11 @@
  * @key: The key to get the index of.
  * @size: The size of the array of the hash table.
  *
- * Return: The index of the key Description: uses djb2 algorithm.
+ * Return: The index of the key
+ *
+ * Description: uses djb2 algorithm.
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_dj2(key) % size);
 }
-
